a0cf1f31aa2ae8df8444f88bf889928647c59657
Dani <daniyil.kud@gmail.com>
2023-01-24 19:18:03 +0100

    react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//! Creation of React Elements
-// const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
-// const list = React.createElement(
-//     'ul',
-//     null,
-//     React.createElement('li', null, 'list Item 1'),
-//     React.createElement('li', null, 'list Item 2'),
-//     React.createElement('li', null, 'list Item 3')
-// )
+function Title() {
+    return <h1>Hello React</h1>
+}
+function Content() {
+    return (
+        <React.Fragment>
+            <p>My name is Daniyil</p>
+            <p>I love React</p>
+        </React.Fragment>
+    )
+}
 
-//? Create Elements with JSX
-const h1 = <h1 id="title">Hello React.js</h1>
-
-let a = 10
-let b = 'Hello test'
-
-const list = (
-    <ul>
-        <li>List Item {a + 10 + 10}</li>
-        <li>List Item {b}</li>
-        <li>List Item 3</li>
-    </ul>
-)
-
-const content = (
-    <div>
-        {h1}
-        {list}
-    </div>
-)
+//! Component
+function App() {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
