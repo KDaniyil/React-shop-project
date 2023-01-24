630f1e7523089d036b382dafbc782332493e515b
Dani <daniyil.kud@gmail.com>
2023-01-24 19:00:26 +0100

    rules working with JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//! Creation of React Elements
 // const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
-
 // const list = React.createElement(
 //     'ul',
 //     null,
@@ -10,15 +10,17 @@ import ReactDOM from 'react-dom/client'
 //     React.createElement('li', null, 'list Item 2'),
 //     React.createElement('li', null, 'list Item 3')
 // )
-//! Creation of React Elements
 
 //? Create Elements with JSX
 const h1 = <h1 id="title">Hello React.js</h1>
 
+let a = 10
+let b = 'Hello test'
+
 const list = (
     <ul>
-        <li>List Item 1</li>
-        <li>List Item 2</li>
+        <li>List Item {a + 10 + 10}</li>
+        <li>List Item {b}</li>
         <li>List Item 3</li>
     </ul>
 )
