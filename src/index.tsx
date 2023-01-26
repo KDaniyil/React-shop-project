c28830d35ddcb5a0c074d1a93d6a9edf736e7473
Dani <daniyil.kud@gmail.com>
2023-01-26 18:48:17 +0100

    individual export

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { type } from '@testing-library/user-event/dist/type'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content'
+import { Content } from './Content'
 
 interface ITItleProps {
     text: string
