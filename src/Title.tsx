aca4b09b715a57ccb32f8af2ba556051d57c0f8f
Dani <daniyil.kud@gmail.com>
2023-01-26 19:39:49 +0100

    delete files and add Absolute Imports

--- src/Title.tsx
+++
@@ -1,6 +0,0 @@
-type Props = {
-    text: string
-}
-export const Title = (props: Props) => {
-    return <h1>Hello {props.text}</h1>
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,6 @@
 import { type } from '@testing-library/user-event/dist/type'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Content } from './Content'
-import { Title } from './Title'
 
-function App() {
-    return (
-        <>
-            <Title text="JS" />
-            <Title text="React" />
-            <Title text="TS" />
-            <Content text1="I love React" text2="I love TS" year={2023} />
-        </>
-    )
-}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
