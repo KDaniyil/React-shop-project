5ce045da68469f9bb3e4427ab762815308a83c19
Dani <daniyil.kud@gmail.com>
2023-01-26 18:58:08 +0100

    add style...

---
+++ src/Title.tsx
@@ -0,0 +1,6 @@
+type Props = {
+    text: string
+}
+export const Title = (props: Props) => {
+    return <h1>Hello {props.text}</h1>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,13 @@ import { type } from '@testing-library/user-event/dist/type'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Content } from './Content'
+import { Title } from './Title'
 
-interface ITItleProps {
-    text: string
-}
-
-const Title = (props: ITItleProps) => {
-    console.log(props)
-    return <h1>Hello {props.text}</h1>
-}
-
-//! Component
 function App() {
     return (
         <>
-            <Title text="React" />
             <Title text="JS" />
+            <Title text="React" />
             <Title text="TS" />
             <Content text1="I love React" text2="I love TS" year={2023} />
         </>
