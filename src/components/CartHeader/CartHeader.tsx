9407794fa7cdf1216da03fe4fad2fdec17e8a2cb
Dani <daniyil.kud@gmail.com>
2023-02-21 19:39:56 +0100

    add CartProductList

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,24 +1,14 @@
+import CartProductList from 'components/CartProductList/CartProductList'
 import CartTotal from 'components/CartTotal/CartTotal'
 import { ProductsInCart } from 'container/App/App'
-import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+
 type Props = {
     productsInCart: ProductsInCart
-    productsObject?: {
-        [id: number]: Product
-    }
 }
-function CartHeader({
-    productsInCart,
-    productsObject = getProductsObject(productsArray),
-}: Props) {
+function CartHeader({ productsInCart }: Props) {
     return (
         <div>
-            {Object.keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productsObject[parseInt(productId)].title} :{' '}
-                    {productsInCart[parseInt(productId)]}
-                </div>
-            ))}
+            <CartProductList productsInCart={productsInCart} />
             <CartTotal productsInCart={productsInCart} />
         </div>
     )
---
+++ src/components/CartProductList/CartProductList.tsx
@@ -0,0 +1,25 @@
+import { ProductsInCart } from 'container/App/App'
+import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+
+type Props = {
+    productsInCart: ProductsInCart
+    productsObject?: {
+        [id: number]: Product
+    }
+}
+const CartProductList = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
+    return (
+        <div>
+            {Object.keys(productsInCart).map((productId) => (
+                <div key={productId}>
+                    {productsObject[parseInt(productId)].title} :{' '}
+                    {productsInCart[parseInt(productId)]}
+                </div>
+            ))}
+        </div>
+    )
+}
+export default CartProductList
