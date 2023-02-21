a92280d6225ea43977a097d2d8e8e66fdc724510
Dani <daniyil.kud@gmail.com>
2023-02-21 20:10:10 +0100

    add cardproductlist

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -1,6 +1,7 @@
 import CartProductList from 'components/CartProductList/CartProductList'
-import CartTotal from 'components/CartTotal/CartTotal'
 import { ProductsInCart } from 'container/App/App'
+import CartProductListItemExtended from 'components/CartProductList/CartProductListItemExtended'
+import { Grid, Typography } from '@mui/material'
 
 type Props = {
     productsInCart: ProductsInCart
@@ -8,8 +9,21 @@ type Props = {
 const CartPage = ({ productsInCart }: Props) => {
     return (
         <div>
-            <CartProductList productsInCart={productsInCart} />
-            <CartTotal productsInCart={productsInCart} />
+            <Typography
+                variant="h4"
+                component="h1"
+                sx={{
+                    marginBottom: '30px',
+                }}
+            >
+                Cart
+            </Typography>
+            <Grid container spacing={4}>
+                <CartProductList
+                    productsInCart={productsInCart}
+                    CartItem={CartProductListItemExtended}
+                />
+            </Grid>
         </div>
     )
 }
