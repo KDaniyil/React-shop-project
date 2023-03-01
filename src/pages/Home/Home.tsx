c83d18075bbd2c6ec93895c22f1dc63a916d72ee
Dani <daniyil.kud@gmail.com>
2023-03-01 19:07:34 +0100

    add reviews

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import ProductsList from 'components/ProductsList/ProductsList'
+import Reviews from 'components/Reviews/Reviews'
 
 type Props = {
     addProductToCart: (count: number, price: number) => void
@@ -7,6 +8,7 @@ function Home({ addProductToCart }: Props) {
     return (
         <div>
             <ProductsList addProductToCart={addProductToCart} />
+            <Reviews />
         </div>
     )
 }
