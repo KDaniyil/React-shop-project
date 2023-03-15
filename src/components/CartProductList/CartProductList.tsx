959ad34ee0280a38489f7f82280815866e91ceda
Dani <daniyil.kud@gmail.com>
2023-03-15 23:19:39 +0100

    add products to store with Redux Thunk

--- src/components/CartProductList/CartProductList.tsx
+++ src/components/CartProductList/CartProductList.tsx
@@ -1,20 +1,24 @@
-import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+import { useAppSelector } from 'redux/hooks'
+import  { getProductsObject, Product } from 'utils/productsArray'
 import CartProductListItem from './CartProductListItem'
 
 type Props = {
     productsInCart: {
         [id: number]: number
     }
-    productsObject?: {
-        [id: number]: Product
-    }
     CartItem?: any
+}
+type ProductsObject = {
+    [id: number]: Product
 }
 const CartProductList = ({
     productsInCart,
-    productsObject = getProductsObject(productsArray),
     CartItem = CartProductListItem,
 }: Props) => {
+
+    const productsArray = useAppSelector((state)=> state.products)
+    const productsObject: ProductsObject = getProductsObject(productsArray)
+
     return (
         <>
             {Object.keys(productsInCart).map((productId) => (
